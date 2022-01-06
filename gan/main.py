fixed train_batch generator gradients

gan/main.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 from matplotlib import pyplot
-from tensorflow.python.keras import optimizers
 import gan
+
 
 latent_dim = 128
 batch_sz = 128
 epochs = 3
-generator = gan.generator
-discriminator = gan.discriminator
+
+generator = gan.generator()
+discriminator = gan.discriminator()
+
+
 loss_func = keras.losses.BinaryCrossentropy(from_logits=True)
 d_optimizer = keras.optimizers.Adam(0.003)
 g_optimizer = keras.optimizers.Adam(0.004)
@@ -17,12 +20,15 @@ g_optimizer = keras.optimizers.Adam(0.004)
 (x_train, _), (x_test, _) = keras.datasets.fashion_mnist.load_data()
 combined_imgs = tf.concat([x_train, x_test], axis=0)
 # normalize and reshape (for conv2D)
-combined_imgs /= 255.0
+combined_imgs = tf.cast(combined_imgs, tf.float32) / 255.0
 combined_imgs = tf.reshape(combined_imgs, [-1, 28,28,1])
 # make TF dataset obj and batch
 dataset = tf.data.Dataset.from_tensor_slices(combined_imgs)
 dataset = dataset.batch(batch_sz, drop_remainder=True)
 
+
+
+#  ============  training loop for single batch ===============
 def train_batch(real_imgs): # batch of real imgs
     # sample random latent vects from N(0,1)
     noise = tf.random.normal([batch_sz, latent_dim])
@@ -41,11 +47,10 @@ def train_batch(real_imgs): # batch of real imgs
 
     # sample points for generator training with deceptive labels (1=fake) and ALL generated fake images are 'real' in disc's perspective
     noise = tf.random.normal([batch_sz, latent_dim])
-    fake_imgs = generator(noise)
     deceptive_labels = tf.ones([batch_sz, 1]) # labels all 1 (want to fool disc)
     # train generator SEPARATELY (don't touch discrim, no access to real data)
     with tf.GradientTape() as tape:
-        predictions = discriminator(fake_imgs)
+        predictions = discriminator(generator(noise))
         g_loss = loss_func(deceptive_labels, predictions)
     grads = tape.gradient(g_loss, generator.trainable_variables)
     g_optimizer.apply_gradients(zip(grads, generator.trainable_variables))
@@ -53,12 +58,21 @@ def train_batch(real_imgs): # batch of real imgs
     return d_loss, g_loss, fake_imgs
 
 
-# main training loop
+# ==================   main training loop   =====================
+for epoch in range(epochs):
+    print("Epoch: ", epoch)
+    # iterate over batches
+    for batch_num, real_img_batch in enumerate(dataset):
+        print("batch: ", batch_num)
+        d_loss, g_loss, fake_imgs = train_batch(real_img_batch)
+        if batch_num % 5 == 0:
+            print("D loss: ", d_loss)
+            print("G loss: ", g_loss)
 
 # save weights
 generator.save("trained_generator")
 
-
+# ===================      inference       ======================
 # generate new images 
 # for i in range(1,50):
 #     pyplot.subplot(5, 10, i)